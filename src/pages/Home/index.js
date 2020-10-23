436c1da8935aff8c8fe798463ff084edb0ece33d
fabiofjunior <fabiofjunior@usp.br>
2020-10-23 07:51:26 -0300

    client2

---
+++ src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { PaginaInteira, Login, LadoEsquerdo, Texto, Campos, LadoDireito} from './styles';
+import { Button, TextField } from '@material-ui/core';
+import doctors from '../../img/doctors.svg';
+
+class Home extends Component{
+    render(){
+        return(
+            <PaginaInteira>
+                <div>
+                    Testando
+                </div>
+            </PaginaInteira>
+
+        );
+    }
+}
+
+export default Home;
