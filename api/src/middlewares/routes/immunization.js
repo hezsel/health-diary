a6234c813febe4d76511743ebc794f96f997ff54
Hezsel <vhesselp@gmail.com>
2020-11-24 18:47:46 -0300

    feat(api): add joi as request validation

--- api/src/middlewares/routes/immunization.js
+++ api/src/middlewares/routes/immunization.js
@@ -6,13 +6,19 @@ const {
   remove,
   list,
 } = require('../../controllers/immunization')
-
+const validator = require('../validator')
+const {
+  immunizationSchema: {
+    create: createSchema,
+    update: updateSchema,
+  },
+} = require('../validator/schemas')
 const router = express.Router()
 
 router
   .get('/', isAuthenticated, list)
-  .post('/', isAuthenticated, create)
-  .put('/', isAuthenticated, update)
+  .post('/', isAuthenticated, validator(createSchema, 'body'), create)
+  .put('/', isAuthenticated, validator(updateSchema, 'body'), update)
   .delete('/', isAuthenticated, remove)
 
 module.exports = router
---
+++ api/src/middlewares/validator/schemas/index.js
@@ -0,0 +1,9 @@
+const userSchema = require('./user')
+const immunizationSchema = require('./immunization')
+const sessionSchema = require('./session')
+
+module.exports = {
+  userSchema,
+  immunizationSchema,
+  sessionSchema,
+}
