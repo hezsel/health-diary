59e49b41dc7ddb8aba65f32f80b2c04745b2612f
Hezsel <vhesselp@gmail.com>
2020-12-04 16:27:09 -0300

    feat(api): add limit on code routes

--- api/src/controllers/services/immunizationCode.js
+++ api/src/controllers/services/immunizationCode.js
@@ -3,6 +3,7 @@ const { generateWhereILike } = require('../../database/utils/filters')
 
 const list = (filters) => ImmunizationCode.findAll({
   where: generateWhereILike(filters),
+  limit: 20,
 })
 
 module.exports = {
